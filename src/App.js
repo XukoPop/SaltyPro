bed448fd04fcaf9dd5ac7180c39d984ed5302134
BukiDev <andrewestepper06@gmail.com>
2025-03-22 14:32:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Help from './pages/Help';
 import Privacy from './pages/Privacy';
