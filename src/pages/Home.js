41e692e25c214ecf749f5c9ec8cfc858f0e88bf7
BukiDev <andrewestepper06@gmail.com>
2025-03-22 14:38:52 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   useEffect(() => {
@@ -263,15 +264,15 @@ function Home() {
                 </svg>
                 Download SaltyPro
               </a>
-              <a 
-                href="/help" 
+              <Link 
+                to="/help" 
                 className="bg-gray-700/80 hover:bg-gray-600/80 text-white font-bold py-4 px-10 rounded-lg transition-all duration-300 flex items-center hover:shadow-lg text-lg"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 Read SaltyBet Guide
-              </a>
+              </Link>
             </div>
           </div>
         </div>
